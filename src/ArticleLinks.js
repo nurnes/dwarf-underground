e3f68a6e46af5ba80d5e37c9ba2405dbe1a66827
Naomi Urnes <naomiurnes@gmail.com>
2017-06-13 16:22:11 -0400

    Merge CommentSection component back into ArticleLinks. Comments appear at the top with timestamp

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -1,22 +1,71 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import './App.css'
-import CommentSection from './CommentSection'
 
-class ArticleLinks extends Component {
+class ArticleLinks extends Component{
+    constructor() {
+        super()
+        this.state = {
+            comments: [],
+            show: false
+        }
+    }
+
+     handleSubmit(ev) {
+        ev.preventDefault()
+        const comments = [...this.state.comments]
+        comments.push(`${(new Date).getMonth()+1}/${(new Date).getDate()}/${(new Date).getFullYear()+1001} ${(new Date).getHours()}:${(new Date).getMinutes()}:${(new Date).getSeconds()} "${this.commentField.value}"`)
+        this.setState({ comments })
+        ev.currentTarget.reset()
+    }
+
+    renderComment(comment, i){
+        return <li key={i}>{comment}</li>
+    }
+
+    changeState(){
+        const bool = !this.state.show
+        this.setState({ show : bool})
+    }
+
     render (){
-        return (
-            <div className="article-links">
-              <a className="article-link" href="#">
-                <i className="fa fa-comments-o"></i>
-                <span className="article-link-text">Comments</span>
-              </a>
-              <CommentSection />
-              <a className="article-link" href="#">
-                <i className="fa fa-share"></i>
-                <span className="article-link-text">Share Post</span>
-              </a>
-            </div>
-        )
+        
+            if(this.state.show){
+                return(
+                    <div className="article-links">
+                        <a className="article-link" onClick={this.changeState.bind(this)}>
+                            <i className="fa fa-comments-o"></i>
+                            <span className="article-link-text">Comments</span>
+                        </a>
+                        <div>
+                            <form onSubmit={this.handleSubmit.bind(this)}>
+                                <input type="text" placeholder="Click here to Write your comment..." ref={(input) => this.commentField = input}/>      
+                            </form>
+                            <ul class="comments">
+                                {this.state.comments.reverse().map(this.renderComment)}
+                            </ul>
+                        </div>
+                        <a className="article-link">
+                            <i className="fa fa-share"></i>
+                            <span className="article-link-text">Share Post</span>
+                        </a>
+                    </div>
+                )
+            }else{
+                return(
+                    <div className="article-links">
+                        <a className="article-link" onClick={this.changeState.bind(this)}>
+                            <i className="fa fa-comments-o"></i>
+                            <span className="article-link-text">Comments</span>
+                        </a>
+                        <p></p>
+                        <a className="article-link">
+                            <i className="fa fa-share"></i>
+                            <span className="article-link-text">Share Post</span>
+                        </a>
+                    </div>
+                )
+            }
+        
     }
 }
 
